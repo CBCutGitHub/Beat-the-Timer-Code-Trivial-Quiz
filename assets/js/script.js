d15b900df38b90e3388a96f9ae04486ef373caeb
MDemp <mdempsey.cbc.ut@gmail.com>
2022-01-05 18:01:32 -0600

    Connected script file and psuecode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Add questions and global variables that reference html elements
+
+// Define time increments
+
+// Start function that hide starts screen, starts timme, and unhide questions
+
+// Function to display questions
+
+// Function to check if question is right or wrong, penalize if wrong, cycle to next questions, and if no more questions, end quiz
+
+// End quiz function
